895438a2e43c9165236e3c07a5bd5fe9d8185d38
Christian_PC <christian.walter@chem-therm.de>
2023-03-22 14:51:55 +0100

    added_tinkerforge

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ChemTherm',
+    version='0.1.0',    
+    description='A Python package used by ChemTherm',
+    url='https://github.com/shuds13/pyexample',
+    author='Christian Walter',
+    author_email='christian.walter@chem-therm.de',
+    license='?',
+    packages=['tinkerforge_lb'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',  
+        'Programming Language :: Python :: 3.11',
+    ],
+)
