a3ad6aacac12670036fc23715c51d21ce42629d1
AnlagenPC <christian.walter@chem-therm.de>
2023-03-29 19:51:43 +0200

    fixed TimeError, all Stages

--- Coupon_lib.py
+++ Coupon_lib.py
@@ -10,12 +10,12 @@ def heating(t0):
     T_high = 850
     T_low = 20
     run_time = 7200
-    T_calc = T_low + (T_high-T_low) *(run_time) * time().time() -t0
+    T_calc = T_low + (T_high-T_low) *(run_time) * time.time() -t0
     T_set = [T_calc,T_calc,T_calc,T_calc]
     MFC_set[0] = 3750
     MFC_set[1] = 0
     MFC_set[2] = 0
-    t_end = run_time - (time().time -t0)
+    t_end = run_time - (time.time() -t0)
     return T_set, MFC_set, t_end
 
 def Coking(t0):
@@ -26,8 +26,8 @@ def Coking(t0):
     T_low = 850
     heat_time = 3600
     run_time = 3600*6
-    if time().time -t0 < heat_time:
-        T_calc = T_low + (T_high-T_low) *(heat_time) * time().time() -t0
+    if time.time() -t0 < heat_time:
+        T_calc = T_low + (T_high-T_low) *(heat_time) * time.time() -t0
         T_set = [T_calc,T_calc,T_calc,T_calc]
     else:
         T_set = [T_high,T_high,T_high,T_high]
@@ -35,7 +35,7 @@ def Coking(t0):
     MFC_set[0] = 0
     MFC_set[1] = 0
     MFC_set[2] = 833
-    t_end = run_time - (time().time -t0)
+    t_end = run_time - (time.time() -t0)
     return T_set, MFC_set, t_end
 
 def Cooling(t0):
@@ -45,11 +45,31 @@ def Cooling(t0):
     T_high = 850
     T_low = 986
     run_time = 1800
-    T_calc = T_low + (T_high-T_low) *(run_time) * time().time() -t0
+    T_calc = T_low + (T_high-T_low) *(run_time) * time.time() -t0
     T_set = [T_calc,T_calc,T_calc,T_calc]
 
     MFC_set[0] = 3750
     MFC_set[1] = 0
     MFC_set[2] = 0
-    t_end = run_time - (time().time -t0)
+    t_end = run_time - (time.time() -t0)
+    return T_set, MFC_set, t_end
+    
+def Decoking(t0):
+    T_set = [0,0,0,0]
+    MFC_set = [1250,1250,0]
+
+    T_high = 850
+    T_low = 1000
+    run_time = 1800
+    T_calc = T_low + (T_high-T_low) *(run_time) * time.time() -t0
+    T_set = [T_calc,T_calc,T_calc,T_calc]
+
+    t_end = run_time - (time.time() -t0)
+    return T_set, MFC_set, t_end
+
+def SteamTreatment(t0):
+    T_set = [1000,1000,1000,1000]
+    MFC_set = [0,1250,0]
+    run_time = 900
+    t_end = run_time - (time.time() -t0)
     return T_set, MFC_set, t_end
