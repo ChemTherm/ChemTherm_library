9ee71167338b0889714145689dd0389871d2d8ce
AnlagenPC <christian.walter@chem-therm.de>
2023-03-30 11:15:44 +0200

    fixex timecalcError

--- Coupon_lib.py
+++ Coupon_lib.py
@@ -5,16 +5,13 @@ import time
 def heating(t0):
     # Preset Heating Function 20 - 850 °C in 
     T_set = [0,0,0,0]
-    MFC_set = [0,0,0]
+    MFC_set = [3750,0,0]
 
     T_high = 850
     T_low = 20
     run_time = 7200
-    T_calc = T_low + (T_high-T_low) /(run_time) * time.time() -t0
+    T_calc = T_low + (T_high-T_low) /(run_time) * (time.time() -t0)
     T_set = [T_calc,T_calc,T_calc,T_calc]
-    MFC_set[0] = 3750
-    MFC_set[1] = 0
-    MFC_set[2] = 0
     t_end = run_time - (time.time() -t0)
     return T_set, MFC_set, t_end
 
@@ -27,7 +24,7 @@ def Coking(t0):
     heat_time = 3600
     run_time = 3600*6
     if time.time() -t0 < heat_time:
-        T_calc = T_low + (T_high-T_low) /(heat_time) * time.time() -t0
+        T_calc = T_low + (T_high-T_low) /(heat_time) * (time.time() -t0)
         T_set = [T_calc,T_calc,T_calc,T_calc]
     else:
         T_set = [T_high,T_high,T_high,T_high]
@@ -45,7 +42,7 @@ def Cooling(t0):
     T_high = 850
     T_low = 986
     run_time = 1800
-    T_calc = T_low + (T_high-T_low) /(run_time) * time.time() -t0
+    T_calc = T_low + (T_high-T_low) /(run_time) * (time.time() -t0)
     T_set = [T_calc,T_calc,T_calc,T_calc]
 
     MFC_set[0] = 3750
@@ -61,7 +58,7 @@ def Decoking(t0):
     T_high = 850
     T_low = 1000
     run_time = 1800
-    T_calc = T_low + (T_high-T_low) /(run_time) * time.time() -t0
+    T_calc = T_low + (T_high-T_low) /(run_time) * (time.time() -t0)
     T_set = [T_calc,T_calc,T_calc,T_calc]
 
     t_end = run_time - (time.time() -t0)
