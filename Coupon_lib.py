e04f279e1fbc01649a3fadeeab20a71a6814d513
AnlagenPC <christian.walter@chem-therm.de>
2023-04-03 18:35:09 +0200

    Coupon: added Heating450

--- Coupon_lib.py
+++ Coupon_lib.py
@@ -24,6 +24,30 @@ def heating(t0):
 
     return T_set, MFC_set, t_end
 
+def heating_450(t0):
+    # Preset Heating Function 450 - 850 °C in 
+    T_set = [0,0,0,0]
+    MFC_set = [3750,0,0]
+
+    T_low = 450
+    T_high = 850
+    heat_time = 2700
+    run_time = 3600
+    if (time.time() -t0) < heat_time:
+        T_calc = T_low + (T_high-T_low) /(heat_time) * (time.time() -t0)
+        T_set = [T_calc,T_calc,T_calc,T_calc]
+    else:
+        T_set = [T_high,T_high,T_high,T_high]
+
+    t_end = run_time - (time.time() -t0)
+    if (t_end < 0):
+        MFC_set = [3750,0,0]
+        T_set = [T_high,T_high,T_high,T_high]
+
+    return T_set, MFC_set, t_end
+
+
+
 def Coking(t0):
     T_set = [0,0,0,0]
     MFC_set = [0,0,833]
@@ -53,6 +77,26 @@ def Cooling(t0):
     T_high = 850
     heat_time = 900
     run_time = 1800
+    if (time.time() -t0) < heat_time:
+        T_calc = T_low + (T_high-T_low) /(heat_time) * (time.time() -t0)
+        T_set = [T_calc,T_calc,T_calc,T_calc]
+    else:
+        T_set = [T_high,T_high,T_high,T_high]
+        
+    t_end = run_time - (time.time() -t0)
+    if (t_end < 0):
+        MFC_set = [3750,0,0]
+        T_set = [T_high,T_high,T_high,T_high]
+    return T_set, MFC_set, t_end
+
+def Cooling_450(t0):
+    T_set = [0,0,0,0]
+    MFC_set = [3750,0,0]
+
+    T_low = 1000
+    T_high = 450
+    heat_time = 7200
+    run_time = heat_time
     if (time.time() -t0) < heat_time:
         T_calc = T_low + (T_high-T_low) /(heat_time) * (time.time() -t0)
         T_set = [T_calc,T_calc,T_calc,T_calc]
