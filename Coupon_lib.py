e5ea7f78404dfca9e9e621bc00f5563a10362b9a
Christian_PC <christian.walter@chem-therm.de>
2023-07-14 14:54:03 +0200

    fixed Json for Coupon

--- Coupon_lib.py
+++ Coupon_lib.py
@@ -6,14 +6,28 @@ def json_timing(config, section, t0):
     MFC_set = [0,0,0]
     
     T_set[0] = config['TIMING']['R1-Z1'][section-1] + (config['TIMING']['R1-Z1'][section] - config['TIMING']['R1-Z1'][section-1]) / (config['TIMING']['t'][section] ) * (time.time() - t0) 
-    T_set[1] = config['TIMING']['R1-Z1'][section-1] + (config['TIMING']['R1-Z1'][section] - config['TIMING']['R1-Z1'][section-1]) / (config['TIMING']['t'][section] ) * (time.time() - t0)
-    T_set[2] = config['TIMING']['R1-Z1'][section-1] + (config['TIMING']['R1-Z1'][section] - config['TIMING']['R1-Z1'][section-1]) / (config['TIMING']['t'][section] ) * (time.time() - t0)
-    T_set[3] = config['TIMING']['R1-Z1'][section-1] + (config['TIMING']['R1-Z1'][section] - config['TIMING']['R1-Z1'][section-1]) / (config['TIMING']['t'][section] ) * (time.time() - t0)
+    T_set[1] = config['TIMING']['R1-Z2'][section-1] + (config['TIMING']['R1-Z2'][section] - config['TIMING']['R1-Z2'][section-1]) / (config['TIMING']['t'][section] ) * (time.time() - t0)
+    T_set[2] = config['TIMING']['R1-Z3'][section-1] + (config['TIMING']['R1-Z3'][section] - config['TIMING']['R1-Z3'][section-1]) / (config['TIMING']['t'][section] ) * (time.time() - t0)
+    T_set[3] = config['TIMING']['R1-Z4'][section-1] + (config['TIMING']['R1-Z4'][section] - config['TIMING']['R1-Z4'][section-1]) / (config['TIMING']['t'][section] ) * (time.time() - t0)
 
     MFC_set[0] = config['TIMING']['MFC-N2'][section]
     MFC_set[1] = config['TIMING']['MFC-Air'][section]
     MFC_set[2] = config['TIMING']['MFC-Ethan'][section]
-    return T_set, MFC_set
+
+    run_time = 0   
+    for i in range(0,config['TIMING']['NoSections']):
+        run_time += config['TIMING']['t'][i]
+    t_end = run_time - (time.time() -t0)
+    
+    section_time = config['TIMING']['t'][0]  
+    for i in range(0,section):
+        section_time += config['TIMING']['t'][i]    
+    t_section = section_time - (time.time() -t0)
+    
+    if t_section < 0:
+        section +=1
+
+    return T_set, MFC_set, t_end, section, t_section
 
 def heating(t0):
     # Preset Heating Function 20 - 850 °C in 
